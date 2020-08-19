d594e88533da5c79c2a945c5756f2f40c6721b6e
VieYang <yangchenzhong@gmail.com>
2020-08-20 00:12:56 +0800

    Release v1.1.0

--- newchain.sh
+++ newchain.sh
@@ -6,7 +6,7 @@ set -eu
 # Use USE_NEWCHAIN_VERSION to specify a specific release version.
 #   Example: USE_NEWCHAIN_VERSION=v1.8.26 ./newchain.sh
 
-newchain_deploy_latest_version='c03125d'
+newchain_deploy_latest_version='v1.1.0'
 default_networkname='mainnet'
 
 function color() {
