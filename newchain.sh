2dfca74c8c9e0785a75524966a097b2c7f166b25
VieYang <yangchenzhong@gmail.com>
2020-09-02 17:08:42 +0800

    guard: add guard for newchain

--- newchain.sh
+++ newchain.sh
@@ -64,6 +64,20 @@ color "32" "Current NewChain network is ${networkname}"
 
 download_rooturl="https://release.cloud.diynova.com"
 
+
+################## work directory ##################
+color "37" "Trying to init the work directory..."
+mkdir -p /data/newchain/${networkname}/bin/
+chown -R $sudo_user /data/newchain
+# check running
+if [ "$(supervisorctl status newchain | awk '{print $2}')" == "RUNNING" ]; then
+  supervisorctl stop newchain
+fi
+if [ "$(supervisorctl status newchainguard | awk '{print $2}')" == "RUNNING" ]; then
+  supervisorctl stop newchainguard
+fi
+
+color "" "################## NewChain ##################"
 function get_newchain_version() {
     if [[ -n ${USE_NEWCHAIN_VERSION:-} ]]; then
         readonly reason="specified in \$USE_NEWCHAIN_VERSION"
@@ -81,8 +95,6 @@ function get_newchain_version() {
     fi
 }
 
-
-
 get_newchain_version
 color "37" "Latest NewChain version is $newchain_version."
 
@@ -98,8 +110,7 @@ if [[ -f /data/newchain/${networkname}/bin/${newchian_network_file} ]]; then
         exit 1
       }
     fi
-
-    exit 0
+    # exit 0 # not now
 fi
 
 file="geth"
@@ -135,9 +146,6 @@ else
   exit 1
 fi
 
-color "37" "Trying to init the work directory..."
-mkdir -p /data/newchain/${networkname}/bin/
-chown -R $sudo_user /data/newchain
 chmod +x $file
 cp $file /data/newchain/${networkname}/bin/${newchian_network_file}
 ln -sf "${newchian_network_file}" /data/newchain/${networkname}/bin/geth || {
@@ -146,7 +154,84 @@ ln -sf "${newchian_network_file}" /data/newchain/${networkname}/bin/geth || {
 }
 color "37" "Updated NewChain binary link."
 
+################## NewChain Guard ##################
+color "" "################## NewChain Guard ##################"
+# install newchain guard
+function get_newchain_guard_version() {
+    if [[ -n ${USE_NEWCHAIN_GUARD_VERSION:-} ]]; then
+        readonly guard_reason="specified in \$USE_NEWCHAIN_GUARD_VERSION"
+        readonly newchain_guard_version="${USE_NEWCHAIN_GUARD_VERSION}"
+    else
+        # Find the latest NewChain Guard version available for download.
+        readonly guard_reason="automatically selected latest available version"
+        newchain_guard_version_url="${download_rooturl}/newton/NewChainGuard/latest.txt"
+        color '' "Trying get newchain guard version from url $newchain_guard_version_url"
+        newchain_guard_version=$(curl -f -s "${newchain_guard_version_url}") || (color "31" "Get NewChain Guard latest version error." && exit 1)
+        readonly newchain_guard_version
+    fi
+}
+
+get_newchain_guard_version
+color "37" "Latest NewChain Guard version is ${newchain_guard_version}."
+
+newchian_guard_network_file="guard.${newchain_guard_version}"
+if [[ -f /data/newchain/${networkname}/bin/${newchian_guard_network_file} ]]; then
+    color "32" "NewChain Guard is up to date."
+    if [[ "$(realpath /data/newchain/${networkname}/bin/guard)" != "/data/newchain/${networkname}/bin/${newchian_guard_network_file}" ]]; then
+      ln -sf "${newchian_guard_network_file}" /data/newchain/${networkname}/bin/guard
+      color "37" "Updated NewChain Guard binary link."
+      # supervisorctl restart newchainguard || {
+      #   color "31" "Failed to restart newchain guard by supervisor."
+      #   exit 1
+      # }
+    fi
+
+    # exit 0 # not now
+fi
+
+guard_file="NewChainGuard"
+function download_guard_bin() {
+  color "34" "Downloading NewChainGuard@${newchain_guard_version} binary to ${guard_file}"
+  color "33" "${download_rooturl}/newton/NewChainGuard/${newchain_guard_version}/${system}/${guard_file}"
+  curl -L "${download_rooturl}/newton/NewChainGuard/${newchain_guard_version}/${system}/${guard_file}" -o $guard_file || {
+    color "31" "Failed to download the NewChain Guard binary."
+    exit 1
+  }
+}
+
+curl --silent -L "${download_rooturl}/newton/NewChainGuard/${newchain_guard_version}/${system}/${guard_file}.sha256" -o "${guard_file}.sha256"
+if test -f "$guard_file"; then
+  sha256sum_res=$(shasum -a 256 -c "${guard_file}.sha256" | awk '{print $2}')
+  if [ "$sha256sum_res" == "OK" ]; then
+      color "32" "Verify $file $sha256sum_res, checksum match."
+  else
+    download_guard_bin
+  fi
+else
+  download_guard_bin
+fi
+
+
+color "37" "Trying to verify the downloaded NewChain Guard binary file..."
+sha256sum_res=$(shasum -a 256 -c "${guard_file}.sha256" | awk '{print $2}')
+if [ "$sha256sum_res" == "OK" ]; then
+  color "32" "Verify $guard_file $sha256sum_res, checksum match."
+else
+  color "41" "Verify $guard_file $sha256sum_res, checksum did NOT match."
+  exit 1
+fi
+
+chmod +x $guard_file
+cp $guard_file /data/newchain/${networkname}/bin/${newchian_guard_network_file}
+ln -sf "${newchian_guard_network_file}" /data/newchain/${networkname}/bin/guard || {
+  color "31" "Failed to link $newchian_guard_network_file to guard."
+  exit 1
+}
+color "37" "Updated NewChain Guard binary link."
+
+################## deploy files ##################
 # NewChain Deploy file
+color "" "################## deploy config files ##################"
 if [[ ! -x /data/newchain/conf/node.toml ]]; then
   newchain_network_deploy_file="newchain-${networkname}-$newchain_deploy_latest_version.tar.gz"
 
@@ -187,34 +272,61 @@ if [[ ! -x /data/newchain/${networkname}/nodedata/geth/ ]]; then
     color "31" "Failed to init the NewChain node data directory."
     exit 1
   }
-  chown -R $sudo_user /data/newchain
+else
+  # force re-init nodedata
+  color "37" "Trying to re-init the NewChain node data directory..."
+  /data/newchain/${networkname}/bin/geth --config /data/newchain/${networkname}/conf/node.toml --datadir /data/newchain/${networkname}/nodedata init /data/newchain/${networkname}/share/newchain${networkname}.json  || {
+    color "31" "Failed to init the NewChain node data directory."
+    exit 1
+  }
 fi
 
-color "37" "Trying to install supervisor..."
+chown -R $sudo_user:$sudo_user /data/newchain/${networkname}
+
+color "37" "Trying to check and configure supervisor..."
 type supervisorctl &> /dev/null || (apt update && apt install -y supervisor) || {
   color "31" "Failed to install supervisor."
   exit 1
 }
 
 sed -i "s/run_as_username/$sudo_user/g" /data/newchain/${networkname}/supervisor/newchain.conf || {
-  color "31" "Failed to update supervisor config file."
+  color "31" "Failed to update newchain supervisor config file."
   exit 1
 }
 cp /data/newchain/${networkname}/supervisor/newchain.conf /etc/supervisor/conf.d/ || {
-  color "31" "Failed to copy supervisor config file."
+  color "31" "Failed to copy newchain supervisor config file."
   exit 1
 }
 
-# check running
-if [ "$(supervisorctl status | grep newchain | awk '{print $2}')" == "RUNNING" ]; then
-  supervisorctl update || {
-    color "31" "Failed to exec supervisorctl update."
+sed -i "s/run_as_username/$sudo_user/g" /data/newchain/${networkname}/supervisor/newchainguard.conf || {
+  color "31" "Failed to update newchain supervisor config file."
+  exit 1
+}
+cp /data/newchain/${networkname}/supervisor/newchainguard.conf /etc/supervisor/conf.d/ || {
+  color "31" "Failed to copy newchain supervisor config file."
+  exit 1
+}
+
+supervisorctl update || {
+  color "31" "Failed to exec supervisorctl update."
+  exit 1
+}
+
+# force sleep 3s, waiting newchain and guard to be stared
+sleep 3
+
+newchainstatus="$(supervisorctl status newchain | awk '{print $2}')"
+if [[ "${newchainstatus}" != "RUNNING" && "${newchainstatus}" != "STARTING" ]]; then
+  supervisorctl start newchain || {
+    color "31" "Failed to exec supervisorctl start newchain."
     exit 1
   }
-  supervisorctl restart newchain
-else
-  supervisorctl update || {
-    color "31" "Failed to exec supervisorctl update."
+fi
+
+newchainguardstatus="$(supervisorctl status newchainguard | awk '{print $2}')"
+if [[ "${newchainguardstatus}" != "RUNNING" && "${newchainguardstatus}" != "STARTING" ]]; then
+  supervisorctl start newchainguard || {
+    color "31" "Failed to exec supervisorctl start newchainguard."
     exit 1
   }
 fi
